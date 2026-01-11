81ce0cc39df58c1c5975e5791d89c8652362583e
hanz <hi@hanz.dev>
2026-01-11 23:09:24 +0700

    Add ERC-20 ABI constants helper file
    - Create mcp_blockchain_rail/erc20_abi.py with ERC20_ABI constant
    - Contains standard ERC-20 functions: name, symbol, decimals, totalSupply, balanceOf
    - Clean separation of concerns to help with type checking

---
+++ mcp_blockchain_rail/erc20_abi.py
@@ -0,0 +1,39 @@
+"""ERC-20 ABI Constants"""
+
+ERC20_ABI = [
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "name",
+        "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+        "type": "function",
+    },
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+        "type": "function",
+    },
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [{"internalType": "uint8", "name": "", "type": "uint8"}],
+        "type": "function",
+    },
+    {
+        "inputs": [{"internalType": "address", "name": "account", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+    {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+]
